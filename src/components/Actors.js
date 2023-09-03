3491cc99147a12085608c0195d9696948a4b7fcc
robertnjunge <mubearobert@gmail.com>
2023-09-03 20:12:44 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,26 @@
+
+
 import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (<div>
+    {/*{code here}*/}
+    <h1>Actors Page</h1>
+    {actors.map((actor) => {
+      return (
+        <div key={actor.name}>
+          <h3>Name: {actor.name}</h3>
+          Movies:
+          <ul>
+            {actor.movies.map((movie) => (
+              <li key={movie}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      )
+    })}
+  </div>);
 }
 
 export default Actors;
